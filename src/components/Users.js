a153f70c7ff3055c80ec161fbff0badf1eb710b7
JSONRete <jsonrete@gmail.com>
2021-10-17 03:06:50 -0400

    commit tree a5912f72347a127d974495b25c5d00802b80a5d3

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 // add any needed imports here
+import { connect } from 'react-redux'
 class Users extends Component {
 
   render() {
@@ -8,7 +9,7 @@ class Users extends Component {
         <ul>
           Users!
           {/* Write code here that displays the usernames of all users in the Redux store */}
-          {/* In addition, display the total number of users curently in the store */}
+          {/* In addition, display the total number of users currently in the store */}
         </ul>
       </div>
     )
@@ -18,4 +19,12 @@ class Users extends Component {
 //add mapStateToProps here
 
 // connect this component to Redux
-export default Users
+// export default Users
+
+
+const mapStateToProps = (state) => {
+  // debugger;
+  return { users: state.users};
+};
+
+export default connect(mapStateToProps)(Users);
